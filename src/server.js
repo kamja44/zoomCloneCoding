9ce77f50f3eb1d4d3d09e18533c830420c97c0a7
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-10-14 15:53:34 +0900

    Setting2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-console.log("Hello");
-console.log(`connect to localhost:${port}`)
+// view engine을 pug로 설정
+app.set("view engine","pug");
+// views 폴더 위치 설정
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+
+console.log(`connect to http://localhost:${port}`)
 app.listen(port);
