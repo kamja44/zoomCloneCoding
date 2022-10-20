31e841ca9e3e995ca1dab747cb3e72c01f4e5ad5
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-10-20 13:46:26 +0900

    setting Nickname socket 2
    JSON.stringify = jS Object -> String
    JSON.parse = String -> JS Object

--- src/server.js
+++ src/server.js
@@ -23,16 +23,27 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"]="Anon"
     console.log("Connected to Browser");
     socket.on("close", () => {
         console.log("DisConnected from Browser");
     });
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => {
-            aSocket.send(message.toString());
+    socket.on("message", (msg) => {
+            const message = JSON.parse(msg);
+            switch(message.type){
+                case "new_message":
+                    sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                    break;
+                case "nickname":
+                    socket["nickname"]=message.payload;
+            }
+            // if(parsed.type === "new_message"){
+            //     sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+            // }else if(parsed.type === "nickname"){
+            //     console.log(parsed.payload);
+            // }
         });
     });
-});
 
 // app.listen
 server.listen(port, handleListen)
