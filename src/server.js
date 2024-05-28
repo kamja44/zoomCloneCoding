a93698da14ce123f96267f73a72067102a5a7f92
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-05-28 13:56:09 +0900

    #1.5

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
 import express from "express";
-import SocketIO from "socket.io";
+import WebSocket from "ws";
 import http from "http";
 
 const app = express();
 const port = 3000;
+
 // view engine을 pug로 설정
-app.set("view engine","pug");
+app.set("view engine", "pug");
+
 // views 폴더 위치 설정
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname+"/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-
 const handleListen = () => console.log(`connect to http://localhost:${port}`);
 
-const httpserver = http.createServer(app);
-const wsServer=SocketIO(httpserver);
-wsServer.on("connection", (socket) => {
-    console.log(socket);
-})
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!!");
+});
 
-httpserver.listen(port, handleListen)
+server.listen(port, handleListen);
